90be74a83a0241dc529ef4b84d715f122ff5d4ff
Hero Liu <lhard@qq.com>
2017-03-14 14:12:24 +0800

    项目源文件

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    console.log('应用全局载入事件');
+  },
+  getUserInfo:function(cb){
+    console.log('全局函数');
+  },
+  globalData:{
+    userInfo:'ltq'
+  }
+})
