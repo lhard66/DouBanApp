cd67f822704604aa9a24112ef6bf6adcb8001d3e
Hero Liu <lhard@qq.com>
2017-03-18 11:21:16 +0800

    gulp自动化工具，初步完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
 const gulp=require('gulp');
+const uglify=require('gulp-uglify');
+const sass=require('gulp-sass');
+const rename=require('gulp-rename');
+const mincss=require('gulp-minify-css');
+const minhtml=require('gulp-minify-html');
 
 gulp.task('default',()=>{
 	console.log('hello world');
+});
+
+//js:最小化
+gulp.task('script',()=>{
+	gulp.src('src/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+//css:转为css、最小化、重命名
+gulp.task('style',()=>{
+	gulp.src('src/**/*.scss')
+		.pipe(sass())
+		.pipe(mincss())
+		.pipe(rename({extname:'.wxss'}))
+		.pipe(gulp.dest('dist'));
+})
+//html:最小化
+gulp.task('html',()=>{
+	gulp.src('src/**/*.wxml')
+		.pipe(minhtml())
+		.pipe(gulp.dest('dist'));
+})
+gulp.task('watch',  () => {
+  gulp.watch('src/**/*.js', ['script']);
+  gulp.watch('src/**/*.scss',['style']);
+  gulp.watch('src/**/*.wxml',['html']);
 });
