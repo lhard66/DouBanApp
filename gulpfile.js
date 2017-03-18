8cec8db5cb3e938e7c9bbf2f0d4d645aa91888e5
Hero Liu <lhard@qq.com>
2017-03-18 17:44:11 +0800

    添加gulp图片复制和json复制压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,50 @@
-const gulp=require('gulp');
-const uglify=require('gulp-uglify');
-const sass=require('gulp-sass');
-const rename=require('gulp-rename');
-const mincss=require('gulp-minify-css');
-const minhtml=require('gulp-minify-html');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const mincss = require('gulp-minify-css');
+const minhtml = require('gulp-minify-html');
+const minjson = require('gulp-jsonminify');
 
-gulp.task('default',()=>{
-	console.log('hello world');
+gulp.task('default', () => {
+  console.log('hello world');
 });
 
 //js:最小化
-gulp.task('script',()=>{
-	gulp.src('src/**/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+gulp.task('script', () => {
+  gulp.src('src/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 //css:转为css、最小化、重命名
-gulp.task('style',()=>{
-	gulp.src('src/**/*.scss')
-		.pipe(sass())
-		.pipe(mincss())
-		.pipe(rename({extname:'.wxss'}))
-		.pipe(gulp.dest('dist'));
-})
+gulp.task('style', () => {
+  gulp.src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(mincss())
+    .pipe(rename({ extname: '.wxss' }))
+    .pipe(gulp.dest('dist'));
+});
 //html:最小化
-gulp.task('html',()=>{
-	gulp.src('src/**/*.wxml')
-		.pipe(minhtml())
-		.pipe(gulp.dest('dist'));
+gulp.task('html', () => {
+  gulp.src('src/**/*.wxml')
+    .pipe(minhtml())
+    .pipe(gulp.dest('dist'));
+});
+//json:最小化
+gulp.task('json', () => {
+  gulp.src('src/**/*.json')
+    .pipe(minjson())
+    .pipe(gulp.dest('dist'));
+});
+//图片:复制过去，不做操作
+gulp.task('img', () => {
+  gulp.src('src/**/**.@(png|jpg|gif|jpeg)')
+    .pipe(gulp.dest('dist'));
 })
-gulp.task('watch',  () => {
+gulp.task('watch', () => {
   gulp.watch('src/**/*.js', ['script']);
-  gulp.watch('src/**/*.scss',['style']);
-  gulp.watch('src/**/*.wxml',['html']);
+  gulp.watch('src/**/*.scss', ['style']);
+  gulp.watch('src/**/*.wxml', ['html']);
+  gulp.watch('src/**/*.json', ['json']);
+  gulp.watch('src/**/*.@(png|jpg|gif|jpeg)', ['img']);
 });
