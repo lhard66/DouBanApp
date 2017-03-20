4843f0ec727c0ebbd3decc500f2014a1c920d255
Hero Liu <lhard@qq.com>
2017-03-20 18:31:09 +0800

    轮播图初步完成，需要配合豆瓣图片进行调整

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,14 +3,18 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World'    
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]    
   },
   //事件处理函数
   bindViewTap: function() {
     console.log('bindViewTap');
     console.log('hello ltq,ht.');
   },
-  onLoad: function () {
+  onLoad: function() {
     console.log('index onLoad');
   }
 })
