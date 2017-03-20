174cfc3f14981b9da9bae4aaca0c8b7497560646
Hero Liu <lhard@qq.com>
2017-03-20 10:31:58 +0800

    添加gulp-load-plugins和配置package.json文件通过npm run启动gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const mincss = require('gulp-minify-css');
-const minhtml = require('gulp-minify-html');
-const minjson = require('gulp-jsonminify');
+// const uglify = require('gulp-uglify');
+// const sass = require('gulp-sass');
+// const rename = require('gulp-rename');
+// const mincss = require('gulp-minify-css');
+// const minhtml = require('gulp-minify-html');
+// const minjson = require('gulp-jsonminify');
+//自动加载package.json中的gulp模块
+plugins=require('gulp-load-plugins')();
+
 
 gulp.task('default', () => {
   console.log('hello world');
@@ -13,27 +16,27 @@ gulp.task('default', () => {
 //js:最小化
 gulp.task('script', () => {
   gulp.src('src/**/*.js')
-    // .pipe(uglify())
+    // .pipe(plugins.uglify())
     .pipe(gulp.dest('dist'));
 });
 //css:转为css、最小化、重命名
 gulp.task('style', () => {
   gulp.src('src/**/*.scss')
-    .pipe(sass())
+    .pipe(plugins.sass())
     // .pipe(mincss())
-    .pipe(rename({ extname: '.wxss' }))
+    .pipe(plugins.rename({ extname: '.wxss' }))
     .pipe(gulp.dest('dist'));
 });
 //html:最小化
 gulp.task('html', () => {
   gulp.src('src/**/*.wxml')
-    // .pipe(minhtml())
+    // .pipe(plugins.minhtml())
     .pipe(gulp.dest('dist'));
 });
 //json:最小化
 gulp.task('json', () => {
   gulp.src('src/**/*.json')
-    // .pipe(minjson())
+    // .pipe(plugins.minjson())
     .pipe(gulp.dest('dist'));
 });
 //图片:复制过去，不做操作
