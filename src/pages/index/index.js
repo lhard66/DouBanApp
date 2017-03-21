b202592794fd0951a0dee7a402ee6e9db8ff063e
Hero Liu <lhard@qq.com>
2017-03-21 20:44:46 +0800

    给小程序的data设置值的时候，一定要记住使用this.setData()方法

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,25 +3,29 @@
 var app = getApp()
 Page({
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ]
+    boards: {url:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'}
   },
   //事件处理函数
   bindViewTap: function() {
     console.log('bindViewTap');
     console.log('hello ltq,ht.');
   },
-  onLoad: function() {    
+  onLoad: function() {
+    var _this=this;
     wx.request({
-      url: 'https://api.douban.com/v2/movie/in_theaters',       
+      url: 'https://api.douban.com/v2/movie/in_theaters',
+      data: {
+        count: 3
+      },
       header: {
         'content-type': 'json'
       },
       success: function(res) {
-        console.log(res.data)
+        console.log(res.data);
+        // _this.data.boards.setData({url:'143912755726.jpg'});
+        _this.setData({boards:'123'});
+        console.log(_this.data.boards);
+        return _this.data.boards;
       }
     })
   }
