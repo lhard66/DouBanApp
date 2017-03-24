90f4e2bb6e38adda23806edc703c27ae0f1c8a11
Hero Liu <lhard@qq.com>
2017-03-24 12:00:34 +0800

    首页board页面基本完成，遗留点击分类名跳转至分类、点击图片或文字跳转至此电影介绍

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -6,8 +6,8 @@ Page({
   data: {
     boards: [
       { key: 'in_theaters' },
-      { key: 'coming_soon1' },
-      { key: 'top2501' }
+      { key: 'coming_soon' },
+      { key: 'top250' }
     ]
   },
   onLoad: function() {
