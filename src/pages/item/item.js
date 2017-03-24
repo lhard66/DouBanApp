41fa129c23843cc7c41ce143887424ec932d1ee5
Hero Liu <lhard@qq.com>
2017-03-24 17:45:12 +0800

    电影内容页面完成

--- src/pages/item/item.js
+++ src/pages/item/item.js
@@ -2,21 +2,23 @@ var app = getApp()
 
 Page({
   data: {
-  	title:'',
+    title: '',
+    casts_num: 0,
+    director_num: 0,
     loading: true,
     movie: {}
   },
-  onLoad: function() {
-    let testId = '26309788'
+  onLoad: function(params) {
+    // let testId = '26309788'
     let _this = this;
     // https://api.douban.com/v2/movie/subject/26309788
-    app.wxfetch(app.URI + 'subject/' + testId)
+    app.wxfetch(app.URI + 'subject/' + params.id)
       .then(res => {
-        _this.setData({ movie: res.data })
+        _this.setData({ movie: res.data, casts_num: res.data.casts.length, director_num: res.data.directors.length })
       })
-      .catch(e=>{
-      	// 处理加载图标和设置title
-      	console.log(e)
+      .catch(e => {
+        // 处理加载图标和设置title
+        console.log(e)
       })
   }
 })
