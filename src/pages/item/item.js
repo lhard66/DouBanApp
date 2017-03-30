6685772f4161a2763d6dc1ce6b3aa60c12a1b57b
Hero Liu <lhard@qq.com>
2017-03-30 18:38:27 +0800

    添加分享功能

--- src/pages/item/item.js
+++ src/pages/item/item.js
@@ -7,8 +7,11 @@ Page({
     director_num: 0,
     loading: true,
     movie: {}
+    // id: -1
   },
   onLoad: function(params) {
+    //记录文章id，用于分享电影
+    // this.setData({ id: params.id })
     wx.showToast(app.loadingConfig)
     let _this = this;
     // https://api.douban.com/v2/movie/subject/26309788
@@ -21,8 +24,27 @@ Page({
         wx.showModal(app.modalConfig)
         console.log(e)
       }).
-      finally(()=>{
-        wx.hideToast()
-      })
+    finally(() => {
+      wx.hideToast()
+    })
+  },
+  onShareAppMessage: function() {
+    let _this = this
+    return {
+      title: '小婷分享给您的电影' + _this.data.movie.title,
+      path: '/pages/item/item?id=' + _this.data.movie.id,
+      success: function(res) {
+        // 分享成功
+        wx.showToast({
+          title: '分享成功'
+        })
+      },
+      fail: function(res) {
+        // 分享失败
+        wx.showToast({
+          title: '分享失败'
+        })
+      }
+    }
   }
 })
